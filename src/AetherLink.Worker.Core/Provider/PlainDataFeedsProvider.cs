7e73ade4933cf18a246800ba28cf5528349938a0
so000up <dragon@aetherlink.network>
2024-06-21 14:57:06 +0800

    Sorted ResponseContent as String.

--- src/AetherLink.Worker.Core/Provider/PlainDataFeedsProvider.cs
+++ src/AetherLink.Worker.Core/Provider/PlainDataFeedsProvider.cs
@@ -1,8 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Grpc.Net.Client.Balancer;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Volo.Abp.DependencyInjection;
 
 namespace AetherLink.Worker.Core.Provider;
@@ -37,12 +41,60 @@ public class PlainDataFeedsProvider : IPlainDataFeedsProvider, ITransientDepende
 
             if (resp.StatusCode != HttpStatusCode.OK) return "";
 
-            return await resp.Content.ReadAsStringAsync();
+            return await GetSortedResponseContentAsStringAsync(resp);
         }
         catch (Exception e)
         {
             _logger.LogError(e, $"Query auth url {url} failed");
             return "";
+        }
+    }
+
+    private async Task<string> GetSortedResponseContentAsStringAsync(HttpResponseMessage response)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+        var jsonObject = JsonConvert.DeserializeObject<JObject>(content);
+        var sortedObject = SortJToken(jsonObject);
+        var sortedJson = JsonConvert.SerializeObject(sortedObject, Formatting.Indented);
+
+        return sortedJson;
+    }
+
+    private JObject SortJToken(JObject jObject)
+    {
+        var sortedDict = new SortedDictionary<string, object>();
+        foreach (var kvp in jObject)
+        {
+            sortedDict[kvp.Key] = SortJToken(kvp.Value);
+        }
+
+        return JObject.FromObject(sortedDict);
+    }
+
+    private object SortJToken(JToken token)
+    {
+        switch (token.Type)
+        {
+            case JTokenType.Object:
+                return SortJToken((JObject)token);
+            case JTokenType.Array:
+                var array = new JArray();
+                foreach (var item in token) array.Add(SortJToken(item));
+
+                return array;
+            case JTokenType.String:
+                return token.ToString();
+            case JTokenType.Integer:
+                return token.ToObject<long>();
+            case JTokenType.Float:
+                return token.ToObject<double>();
+            case JTokenType.Boolean:
+                return token.ToObject<bool>();
+            case JTokenType.Null:
+                return null;
+
+            default:
+                throw new NotSupportedException($"Unsupported JSON token: {token.Type}");
         }
     }
 }
