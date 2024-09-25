c9ff940efd571e86a57deba9782c862afcd87a31
so000up <dragon@aetherlink.network>
2024-09-25 10:47:51 +0800

    add metrics report option

---
+++ src/Aetherlink.PriceServer.HttpApi/Options/MetricsReportOption.cs
@@ -0,0 +1,6 @@
+namespace AetherlinkPriceServer.Options;
+
+public class MetricsReportOption
+{
+    public int Interval { get; set; } = 60000;
+}
--- src/Aetherlink.PriceServer.HttpApi/Worker/MetricsReportWorker.cs
+++ src/Aetherlink.PriceServer.HttpApi/Worker/MetricsReportWorker.cs
@@ -1,22 +1,24 @@
-using System;
 using System.Threading.Tasks;
+using AetherlinkPriceServer.Options;
 using AetherlinkPriceServer.Reporter;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Volo.Abp.BackgroundWorkers;
 using Volo.Abp.Threading;
 
 namespace AetherlinkPriceServer.Worker;
 
-public class DailyMetricsReportWorker : AsyncPeriodicBackgroundWorkerBase
+public class MetricsReportWorker : AsyncPeriodicBackgroundWorkerBase
 {
+    private readonly MetricsReportOption _option;
     private readonly IPriceQueryReporter _reporter;
 
-    public DailyMetricsReportWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory,
-        IPriceQueryReporter reporter) : base(timer, serviceScopeFactory)
+    public MetricsReportWorker(AbpAsyncTimer timer, IServiceScopeFactory serviceScopeFactory,
+        IPriceQueryReporter reporter, IOptions<MetricsReportOption> option) : base(timer, serviceScopeFactory)
     {
         _reporter = reporter;
-        // Timer.Period = (int)TimeSpan.FromDays(1).TotalMilliseconds;
-        Timer.Period = 1000;
+        _option = option.Value;
+        Timer.Period = _option.Interval;
     }
 
     protected override async Task DoWorkAsync(PeriodicBackgroundWorkerContext workerContext)
