d1bd880b2cc95e8c20fca388b60cd5c568ec5c7f
Stanik-13 <stanik.xu@aetherlink.network>
2024-09-27 15:19:18 +0800

    fix:cancle notes

--- src/AetherLink.Worker/AetherLinkServerWorkerModule.cs
+++ src/AetherLink.Worker/AetherLinkServerWorkerModule.cs
@@ -113,9 +113,9 @@ namespace AetherLink.Worker
 
         private void ConfigureBackgroundWorker(ApplicationInitializationContext context)
         {
-            // context.AddBackgroundWorkerAsync<SearchWorker>();
-            // context.AddBackgroundWorkerAsync<UnconfirmedWorker>();
-            // context.AddBackgroundWorkerAsync<LogsPoller>();
+            context.AddBackgroundWorkerAsync<SearchWorker>();
+            context.AddBackgroundWorkerAsync<UnconfirmedWorker>();
+            context.AddBackgroundWorkerAsync<LogsPoller>();
             context.AddBackgroundWorkerAsync<TonIndexerWorker>();
         }
 
