94e78d26e663507e2800e2dab27ea68fa12bb1d8
Stanik-13 <stanik.xu@aetherlink.network>
2024-09-28 22:16:30 +0800

    add configure map

--- src/AetherLink.Worker/AetherLinkServerWorkerModule.cs
+++ src/AetherLink.Worker/AetherLinkServerWorkerModule.cs
@@ -83,6 +83,10 @@ namespace AetherLink.Worker
             Configure<ProcessJobOptions>(configuration.GetSection("ProcessJob"));
             Configure<OracleInfoOptions>(configuration.GetSection("OracleChainInfo"));
             Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "AetherLinkServer:"; });
+            Configure<TonPublicConfigOptions>(configuration.GetSection("Chains:ChainInfos:Ton"));
+            Configure<TonSecretConfigOptions>(configuration.GetSection("OracleChainInfo:ChainConfig:Ton"));
+            Configure<TonGetBlockProviderOptions>(configuration.GetSection("Chains:ChainInfos:Ton:Indexer:GetBlock"));
+            Configure<TonCenterProviderApiConfig>(configuration.GetSection("Chains:ChainInfos:Ton:Indexer:TonCenter"));
         }
 
 
