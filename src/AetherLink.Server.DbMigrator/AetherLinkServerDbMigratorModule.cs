7fe07bd00bcb18d84d9aeb34f12338b04e9e101d
so000up <dragon@aetherlink.network>
2024-12-05 14:01:44 +0800

    add AetherLink.Server.DbMigrator

---
+++ src/AetherLink.Server.DbMigrator/AetherLinkServerDbMigratorModule.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace AetherLink.Server.DbMigrator;
+
+[DependsOn(typeof(AbpAutofacModule))]
+public class AetherLinkServerDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+    }
+}
